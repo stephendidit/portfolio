77cc4ad695d2cb3ce81e41c9e9034bc273cb1def
Stephen Frost <stephendidit@gmail.com>
2016-05-30 21:22:03 -0400

    Separate js from html

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('footer').load( "./footer.html" );
+    $('#navbar').load( "./topnav.html", function(){ // adding this function is callback
+        $('#contactMenu').tooltip();
+    } );
+    
+}).on({
+    mouseenter: function () {
+        $(this).toggleClass('active','fast', 'easeOutSine');
+    }, mouseleave: function() {
+        $(this).toggleClass('active','fast', 'easeOutSine');
+    } }, "#footer .fa"); // passes element as argument to .on
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.navbar-brand').fadeIn('fast', 'easeInSine');
+    } else {
+        $('.navbar-brand').fadeOut('fast','easeOutSine');
+    }
+});
